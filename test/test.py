c68735cbf6945d20912b6464b0fef95c666446c0
louyh <lyh2321@126.com>
2020-11-16 14:37:10 +0800

    fix bug

--- test/test.py
+++ test/test.py
@@ -1,14 +1,33 @@
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor  # 线程池，进程池
-import threading, time
+import pymysql
 
+config = {
+    "host": "127.0.0.1",
+    "port": 10001,
+    "user": "root",
+    "password": "123456",
+    "database": "lexuss"
+}
 
-def test(arg):
-    print(arg, threading.current_thread().name)
-    time.sleep(1)
+db = pymysql.connect(**config)
+cursor = db.cursor()
 
-
-if __name__ == "__main__":
-    thread_pool = ThreadPoolExecutor(5)  # 定义5个线程执行此任务
-    process_pool = ProcessPoolExecutor(5)  # 定义5个进程
-    for i in range(20):
-        thread_pool.submit(test, i)
+db.ping()
+cursor.execute("select userid,chapterid from tmp_louyh_1103_2;")
+results = cursor.fetchall()
+for row in results:
+    db.ping()
+    cursor.execute(
+        "select count(*) from t_lms_userchapterstatistics where userid='%s' and chapterid='%s';" % (row[0], row[1]))
+    count = 0
+    resultss = cursor.fetchall()
+    for rows in resultss:
+        print(rows[0])
+        count = int(rows[0])
+    if (count == 0):
+        db.ping()
+        cursor.execute(
+            "insert into t_lms_userchapterstatistics_s select * from tmp_t_lms_userchapterstatistics_20201103_1 where userid='%s' and chapterid='%s' limit 1;" % (
+                row[0], row[1]))
+        db.commit()
+    if (count > 1):
+        print('id:', row[0], 'chapterid:', row[1])
